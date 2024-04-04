508530cb0483dae6e4ffd9eee4da5f94f584bbe3
thanhquang <thanhquangk22@gmail.com>
2024-04-05 00:25:25 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { RouterProvider } from "react-router-dom";
 import router from "./components/Routes";
 import ChatList from "./components/chat-list/chat-list";
 import Sidebar from "./components/sile-bar/sidebar";
+import { ApolloProvider } from "@apollo/client";
+import client from "./constants/apollo-clients";
 
 const darkTheme = createTheme({
   palette: {
@@ -19,23 +21,24 @@ const darkTheme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-
-      <Container maxWidth={false} disableGutters>
-        <Grid container>
-          <Grid item xs={0} md={0.5} lg={0.5} xl={0.5}>
-            <Sidebar />
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container maxWidth={false} disableGutters>
+          <Grid container>
+            <Grid item xs={0} md={0.5} lg={0.5} xl={0.5}>
+              <Sidebar />
+            </Grid>
+            <Grid item xs={12} md={3} lg={3} xl={3}>
+              <ChatList />
+            </Grid>
+            <Grid item xs={12} md={7.5} lg={7.5} xl={8.5}>
+              <Routes />
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={3} lg={3} xl={3}>
-            <ChatList />
-          </Grid>
-          <Grid item xs={12} md={7.5} lg={7.5} xl={8.5}>
-            <Routes />
-          </Grid>
-        </Grid>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
